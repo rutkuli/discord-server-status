e228c14e0d9d98f221e7bee0359580d061cb6fd5
rutkuli <44347946+rutkuli@users.noreply.github.com>
2020-12-14 02:21:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ client.on('ready', () => {
 			port: config.port
 			}).then((state) => {							 
 			client.user.setActivity(`${state.players.length}/${state.maxplayers} online!`, {type: "PLAYING"});
-	}).catch((error) => {
-			client.user.setActivity(`Currently offline!`, {type: "PLAYING"});
+	     }).catch((error) => {
+		client.user.setActivity(`Currently offline!`, {type: "PLAYING"});
             });
       }, 15000); 		
 });
-
 client.login(config.token);
