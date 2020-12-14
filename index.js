4aa408aada1dc996f4972881938ebc33cd6360df
rutkuli <44347946+rutkuli@users.noreply.github.com>
2020-12-14 02:21:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ client.on('ready', () => {
   console.log(`Status bot, is ready for use.`, "ready");
         setInterval(() => {
             gamedig.query({
-				type: 'rust',
-				host: config.ip,
-				port: config.port
+			type: 'rust',
+			host: config.ip,
+			port: config.port
 			}).then((state) => {							 
-				client.user.setActivity(`${state.players.length}/${state.maxplayers} online!`, {type: "PLAYING"});
-		}).catch((error) => {
+			client.user.setActivity(`${state.players.length}/${state.maxplayers} online!`, {type: "PLAYING"});
+	}).catch((error) => {
 			client.user.setActivity(`Currently offline!`, {type: "PLAYING"});
-});
-		    }, 15000); 		
+            });
+      }, 15000); 		
 });
 
 client.login(config.token);
